7909e2bc6c0fd22f8fcd09d66f8ffbe3482e5150
talkrz <krzysztof.talajko@gmail.com>
2016-12-19 00:57:07 +0100

    Improve code style based on eslint

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,26 +1,26 @@
 const initialState = {
-    isLoggedIn: false,
-    userId: false,
-    accessKey: '',
-    accessKeyExpiresAt: '',
-    refreshKey: '',
-    refreshKeyExpiresAt: ''
-}
+  isLoggedIn: false,
+  userId: false,
+  accessKey: '',
+  accessKeyExpiresAt: '',
+  refreshKey: '',
+  refreshKeyExpiresAt: '',
+};
 
 const user = (state = initialState, action) => {
-    switch(action.type) {
-        case 'SET_ACCESS_CREDENTIALS':
-            return Object.assign({}, state, {
-                userLoggedIn: true,
-                userId: action.userId,
-                accessKey: action.accessKey,
-                accessKeyExpiresAt: action.accessKeyExpiresAt,
-                refreshKey: action.refreshKey,
-                refreshKeyExpiresAt: action.refreshKeyExpiresAt
-            });
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'SET_ACCESS_CREDENTIALS':
+      return Object.assign({}, state, {
+        userLoggedIn: true,
+        userId: action.userId,
+        accessKey: action.accessKey,
+        accessKeyExpiresAt: action.accessKeyExpiresAt,
+        refreshKey: action.refreshKey,
+        refreshKeyExpiresAt: action.refreshKeyExpiresAt,
+      });
+    default:
+      return state;
+  }
+};
 
 export default user;
