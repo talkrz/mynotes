96aa63e99267f17e3779030dec13cbe7211ca027
talkrz <krzysztof.talajko@gmail.com>
2017-01-02 02:01:40 +0100

    Improve editor usability

--- src/components/NoteEditor/TextEditor.js
+++ src/components/NoteEditor/TextEditor.js
@@ -4,6 +4,7 @@ import { Editor } from 'draft-js';
 class TextEditor extends Component {
   constructor(props) {
     super(props);
+    this.focus = () => this.refs.editor.focus();
     this.onChange = (editorState) => {
       this.props.editorContentChanged(editorState);
     };
@@ -11,10 +12,11 @@ class TextEditor extends Component {
 
   render() {
     return (
-      <div className="NoteEditor-text-editor">
+      <div className="NoteEditor-text-editor" onClick={this.focus}>
         <Editor
           editorState={this.props.editorState}
           onChange={this.onChange}
+          ref="editor"
           placeholder="Write a note..." />
       </div>
     );
