29d274f9593ffc61b3c616eac36238f5c73e9485
talkrz <krzysztof.talajko@gmail.com>
2017-01-12 21:10:06 +0100

    Join helper function file

--- src/redux/board/showNotes.js
+++ src/redux/board/showNotes.js
@@ -1,9 +1,27 @@
 import calculateNotesMaxZ from './calculateNotesMaxZ';
-import initializeNotes from './note/initializeNotes';
+import noteInitialState from './note/noteInitialState';
 import calculateNotesViewDimensions from './calculateNotesViewDimensions';
 
 /**
+ * Initialize an array of notes according to data from the server
+ * @param  {Array} notesServerData an array of notes data
+ * @return {Array}                 result notes array
+ */
+function initializeNotes(notesServerData) {
+  const notes = [];
+
+  notesServerData.forEach((noteServerData) => {
+    const note = Object.assign({}, noteInitialState(), noteServerData);
+    notes.push(note);
+  });
+
+  return notes;
+}
+
+/**
  * Add notes to the board state from board data object and returns new state
+ * More specifically apply state which server is not aware of
+ * like notes dimensions in pixels
  * @param  {Object} state old state
  * @param  {Object} board board data
  * @return {Object}       new state
