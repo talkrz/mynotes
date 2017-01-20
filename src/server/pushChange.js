699aa89159c761efb94710ec104e510c0962f607
talkrz <krzysztof.talajko@gmail.com>
2017-01-20 14:54:11 +0100

    Optimize number of server requests
    By grouping changes of the same properties on per note (for instance when editing note only one request per 200ms is sent)

--- src/server/pushChange.js
+++ src/server/pushChange.js
@@ -1,13 +1,45 @@
+import throttle from 'lodash.throttle';
 import saveChange from './saveChange';
 
+let pendingChanges = [];
+let index = {};
+
 /**
- * For now it's only placeholder where additional logic like request
- * throttling or merging should be added
+ * Save all pending changes to the server
+ */
+function pushPendingChanges() {
+  const pendingServerOperations = [];
+  pendingChanges.forEach((change) => {
+    pendingServerOperations.push(saveChange(change));
+    pendingChanges = [];
+    index = {};
+  });
+
+  return Promise.all(pendingServerOperations);
+}
+
+const throttledPushPendingChanges = throttle(() => pushPendingChanges(), 200);
+
+/**
+ * Optimize number of requests for NOTE_UPDATE operations
+ * Other operations are dispatched immediately
  * @param  {string}  operationType operation type
  * @param  {Object}  data          operation data
  * @return {Promise}               operation response
  */
 function pushChange({ operationType, data }) {
+  if (operationType === 'NOTE_UPDATE') {
+    const indexKey = data.id + Object.keys(data).join('');
+
+    if (index[indexKey] === undefined) {
+      pendingChanges.push({ operationType, data });
+      index[indexKey] = pendingChanges.length - 1;
+    } else {
+      pendingChanges[index[indexKey]] = { operationType, data };
+    }
+
+    return throttledPushPendingChanges();
+  }
   return saveChange({ operationType, data });
 }
 
